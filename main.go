cf107beef9926caaac824dc95d2a091442aa5bc1
awe <a@awews.com>
2020-07-09 22:30:57 -0700

    build server on localhost:8000

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	h := http.HandlerFunc(Echo)
+	if err := http.ListenAndServe("localhost:8000", h); err != nil {
+		log.Fatal(err)
+	}
+}
+
+// Echo tells you about the request made
+func Echo(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "You asked to", r.Method, r.URL.Path)
+}
